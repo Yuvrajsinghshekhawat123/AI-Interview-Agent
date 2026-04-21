305c83e29452eba40184dbaa7ca4a92ca02afdcb
Yuvraj Singh Shekhawat <ys456913@gmail.com>
2026-04-21 15:01:12 +0530

    s

---
+++ Frontend/src/05-pages/01-ProtectedPages/02-interview2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Interview2 = () => {
+  return (
+    <div>Interview2</div>
+  )
+}
--- Server/src/server.js
+++ Server/src/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(requestIp.mw());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"http://localhost:5174",
     methods:["GET", "POST", "PUT", "DELETE"],
     credentials:true,
 }));
