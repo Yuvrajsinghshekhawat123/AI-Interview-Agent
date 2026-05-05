8b0ceb5ab96c9800924d629053219544d944313f
Yuvraj singh shekhawat <152900694+Yuvrajsinghshekhawat123@users.noreply.github.com>
2026-05-05 10:03:46 +0530

    Refactor PrivateRoute to use Redux for user state

--- Frontend/src/04-layout/Protected_Routes/01-PrivateRoute.jsx
+++ Frontend/src/04-layout/Protected_Routes/01-PrivateRoute.jsx
@@ -4,21 +4,23 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useLoginUserDetails } from "../../03-features/01-user/03-hook/02-useUserLoginDetails";
 import { ClipLoader } from "react-spinners";
 
+import { useSelector } from "react-redux";
+
 export default function PrivateRoute() {
-    const { data, isLoading } = useLoginUserDetails();
-    
-  
-   if (isLoading) {
+  const user = useSelector((state) => state.user.user);
+  const { data, isLoading, isFetching } = useLoginUserDetails();
+
+  if (isLoading && !user) {
     return (
       <div className="flex justify-center items-center min-h-96">
-        <div className="text-center">
-          <ClipLoader color="#2563eb" loading size={50} />
-        </div>
+        <ClipLoader color="#2563eb" loading size={50} />
       </div>
     );
   }
 
-   
-  if (!data) return <Navigate to="/" replace />;
+  if (!user && !data?.user) {
+    return <Navigate to="/" replace />;
+  }
+
   return <Outlet />;
 }
